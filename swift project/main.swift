20b5b35148d844492c3e06478ac609c5faa04a40
macos <macos@macoss-Mac.local>
2019-02-25 14:03:59 -0800

    Initial Commit

---
+++ swift project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift project
+//
+//  Created by macos on 2019-02-25.
+//  Copyright © 2019 macos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
